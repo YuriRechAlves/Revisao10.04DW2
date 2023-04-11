ce400359c0b55dfbea4105be8dce5e36732c8ac3
Yuri Rech Alves <yurirech2003@gmail.com>
2023-04-10 21:40:41 -0300

    Adicionado get e delete para ser usado no arquivo rotas.js

--- rotas.js
+++ rotas.js
@@ -2,5 +2,19 @@ const express = require("express");
 
 const routes = express.Router();
 
+let lista = [{"id":1,"nome":"Yuri","idade":19},{"id":2,"nome":"Diogo","idade":19},{"id":3,"nome":"Luan","idade":19}]
 
-module.exports = routes
+routes.get("/",(req,res)=>{
+    res.status(200).json(lista)
+});
+
+routes.get("/:id",(req,res)=>{
+    res.status(200).json(lista[req.params.id-1])
+});
+
+routes.delete("/:id",(req,res)=>{
+    lista.splice(req.params.id-1,1);
+    res.status(200).redirect("/")
+})
+
+module.exports = routes;
